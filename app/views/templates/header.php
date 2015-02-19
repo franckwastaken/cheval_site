ab82bbb1422a75bd85099cf3665b2bb5828cc65c
Franck <franck.amofa@gmail.com>
2015-02-19 09:47:05 +0100

    exemple de css

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,6 +3,6 @@
 <head>
     <meta charset="utf-8">
     <title>Home</title>
-    <link rel="stylesheet" href="<?php echo ASSET_ROOT; ?>/css/global.css">
+    <link rel="stylesheet" href="<?php echo ASSET_ROOT; ?>/css/aplli.css">
 </head>
 <body>
